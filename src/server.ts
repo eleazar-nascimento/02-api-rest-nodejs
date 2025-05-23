88acf03b80166ffbe4e6b5b3a7c7db78e6ca3955
Eleazar Nascimento <eleazar.nascimento@gmail.com>
2025-05-23 11:37:13 -0300

    initialize project with Fastify server and TypeScript configuration

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+
+const server = fastify({ logger: true });
+
+server.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+server.listen({ port: 4444 }, (err, address) => {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+  server.log.info(`Server listening at ${address}`);
+});
